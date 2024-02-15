2aaa45f1ec6ed3db8b05ec2f5780d24b93ca0545
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-15 21:59:47 +0000

    Refactor.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home.page';
 
 @NgModule({
     declarations: [
-    HomeComponent
-  ],
+        HomeComponent
+    ],
     imports: [
         CommonModule
     ]
