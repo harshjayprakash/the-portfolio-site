e87299796307cb3289c74bc3c4363e19fcf819bd
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-15 22:21:56 +0000

    Generate home hero component.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { RouterModule } from "@angular/router";
+import { HomeHeroComponent } from './home-hero/home-hero.component';
 
 @NgModule({
     declarations: [
-        HomePage
+        HomePage,
+        HomeHeroComponent
     ],
     imports: [
         CommonModule,
