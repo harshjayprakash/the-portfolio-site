673d9e85e477b8b56cf9b409d4cb10940fcfb7f5
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-15 22:14:02 +0000

    Add child routes.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [
         HomePage
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule.forChild([{ path: "", title: "Home", component: HomePage }])
     ],
-    exports: [HomePage]
+    exports: [RouterModule]
 })
 export class HomeModule { }
