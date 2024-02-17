443cd35602fa059edc4d9e4a2745b23dfa754e57
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-17 14:43:54 +0000

    Generate home skills component.

---
+++ src/app/views/home/home-skills/home-skills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pf-home-skills',
+    templateUrl: './home-skills.component.html',
+    styleUrl: './home-skills.component.css'
+})
+export class HomeSkillsComponent {
+
+}
--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { RouterModule } from "@angular/router";
 import { HomeHeroComponent } from './home-hero/home-hero.component';
+import { HomeSkillsComponent } from './home-skills/home-skills.component';
 
 @NgModule({
     declarations: [
         HomePage,
-        HomeHeroComponent
+        HomeHeroComponent,
+        HomeSkillsComponent
     ],
     imports: [
         CommonModule,
