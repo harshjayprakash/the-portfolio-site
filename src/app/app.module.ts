5a9c5666726708c911acead605539d503bf14a18
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-20 12:35:30 +0000

    Update imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
+import { ViewsModule } from "./views/views.module";
 
 @NgModule({
     declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        CoreModule,
+        SharedModule,
+        ViewsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
