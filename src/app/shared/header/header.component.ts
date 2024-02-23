14870cfd43b2a61d6861bd634e187e2e4d62dffe
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-23 14:10:21 +0000

    Add properties and events.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Routes } from "@angular/router";
+import { ROUTES } from "../../views/views.module";
 
 @Component({
     selector: 'pf-header',
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    showMenu!: boolean;
+    routes_!: Routes;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.showMenu = false;
+        this.routes_ = ROUTES.filter(
+            (route) =>
+                route.title === 'Home'
+        );
+    }
+
+    onNavLinkClick(): void {
+        this.showMenu = false;
+    }
 
 }
