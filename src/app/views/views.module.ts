a3c4b2619eade14651139be27e95cd88f1b0759a
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-02-29 21:23:02 +0000

    Update routes.

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
+
 import { HomeModule } from "./home/home.module";
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { AboutModule } from './about/about.module';
+import { SitemapModule } from './sitemap/sitemap.module';
+import { ValuesModule } from './values/values.module';
+import { ReferencesModule } from './references/references.module';
 
 export const ROUTES: Routes = [
-    { path: "", title: "Home", loadChildren: () => HomeModule }
+    { path: "", title: "Home", loadChildren: () => HomeModule },
+    { path: 'portfolio', title: "Portfolio", loadChildren: () => PortfolioModule },
+    { path: 'about', title: 'About', loadChildren: () => AboutModule },
+    { path: 'sitemap', title: 'Sitemap', loadChildren: () => SitemapModule },
+    { path: 'values', title: 'Values', loadChildren: () => ValuesModule },
+    { path: 'references', title: 'References', loadChildren: () => ReferencesModule },
 ]
 
 @NgModule({
