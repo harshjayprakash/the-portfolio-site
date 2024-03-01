5fabb1f6f20c4fcc3fea7dc9fa627b056bc2f54b
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-01 13:23:05 +0000

    Remove components

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -4,16 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { HomeHeroComponent } from './home-hero/home-hero.component';
-import { HomeSkillsComponent } from './home-skills/home-skills.component';
-import { HomePastProjectsComponent
-    } from './home-past-projects/home-past-projects.component';
 
 @NgModule({
     declarations: [
         HomePage,
-        HomeHeroComponent,
-        HomeSkillsComponent,
-        HomePastProjectsComponent
+        HomeHeroComponent
     ],
     imports: [
         CommonModule,
