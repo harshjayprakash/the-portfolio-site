16a194810c403158076417635c2d0e0afbfa3774
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-01 13:37:58 +0000

    Import and set routes.

--- src/app/views/about/about.module.ts
+++ src/app/views/about/about.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPage } from './about.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
         AboutPage
     ],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            { path: '', title: 'About', component: AboutPage }
+        ])
+    ],
+    exports: [RouterModule]
 })
 export class AboutModule { }
--- src/app/views/portfolio/portfolio.module.ts
+++ src/app/views/portfolio/portfolio.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PortfolioPage } from './portfolio.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
         PortfolioPage
     ],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            { path: '', title: 'Portfolio', component: PortfolioPage }
+        ])
+    ],
+    exports: [RouterModule]
 })
 export class PortfolioModule { }
